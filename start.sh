3d780a6fbdd112a65858aac22a4a38f04512d3d2
Devill Don <53294521+devillD@users.noreply.github.com>
2021-08-09 11:39:28 +0600

    DEV

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-wget $CRED_URL -O credentials.json && wget $TOKEN_URL -O token.pickle && python3 -m bot
+wget $CRED_URL -O credentials.json && wget $TOKEN_URL -O token.pickle && wget $DRIVE_FILE -O drive_folder && python3 -m bot
